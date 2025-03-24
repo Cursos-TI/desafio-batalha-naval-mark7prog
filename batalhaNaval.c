2150737ca7e16397af9b61393925aa833904ae64
mark7prog <marcosservidor.video@gmail.com>
2025-03-24 14:52:53 +0000

    DESAFIO AVENTUREIRO - CONCLUÍDO

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,23 +1,44 @@
 #include <stdio.h>
-
 #define TAMANHO_TABULEIRO 10
 #define TAMANHO_NAVIO 3
 
 int main() {
   int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO] = {0}; // Inicializa o tabuleiro com água (0)
 
-  // Posiciona o navio horizontalmente
+  // Posiciona o primeiro navio horizontalmente
   int navio_horizontal_linha = 2;
   int navio_horizontal_coluna = 3;
-  for (int i = 0; i < TAMANHO_NAVIO; i++) {
-    tabuleiro[navio_horizontal_linha][navio_horizontal_coluna + i] = 3; // 3 representa o navio
+  if (navio_horizontal_coluna + TAMANHO_NAVIO <= TAMANHO_TABULEIRO) {
+    for (int i = 0; i < TAMANHO_NAVIO; i++) {
+      tabuleiro[navio_horizontal_linha][navio_horizontal_coluna + i] = 3;
+    }
   }
 
-  // Posiciona o navio verticalmente
+  // Posiciona o segundo navio verticalmente
   int navio_vertical_linha = 6;
   int navio_vertical_coluna = 1;
-  for (int i = 0; i < TAMANHO_NAVIO; i++) {
-    tabuleiro[navio_vertical_linha + i][navio_vertical_coluna] = 3; // 3 representa o navio
+  if (navio_vertical_linha + TAMANHO_NAVIO <= TAMANHO_TABULEIRO) {
+    for (int i = 0; i < TAMANHO_NAVIO; i++) {
+      tabuleiro[navio_vertical_linha + i][navio_vertical_coluna] = 3;
+    }
+  }
+
+  // Posiciona o primeiro navio diagonal (principal)
+  int navio_diagonal1_linha = 0;
+  int navio_diagonal1_coluna = 0;
+  if (navio_diagonal1_linha + TAMANHO_NAVIO <= TAMANHO_TABULEIRO) {
+    for (int i = 0; i < TAMANHO_NAVIO; i++) {
+      tabuleiro[navio_diagonal1_linha + i][navio_diagonal1_coluna + i] = 3;
+    }
+  }
+
+  // Posiciona o segundo navio diagonal (secundária)
+  int navio_diagonal2_linha = 0;
+  int navio_diagonal2_coluna = 9;
+  if (navio_diagonal2_linha + TAMANHO_NAVIO <= TAMANHO_TABULEIRO) {
+    for (int i = 0; i < TAMANHO_NAVIO; i++) {
+      tabuleiro[navio_diagonal2_linha + i][navio_diagonal2_coluna - i] = 3;
+    }
   }
 
   // Exibe o tabuleiro
